c7eace0b19402848b7eaa034bee04f13d0e9ce98
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-26 12:34:54 +0200

    feat: create glob file if not exist

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/takahiromitsui/go-server-counter/internal/config"
+	"github.com/takahiromitsui/go-server-counter/internal/handlers"
 )
 
 const port = ":8080"
+var app config.AppConfig
 
 func main() {
+	app.FilePath = "requests.gob"
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	file , err := os.Open(app.FilePath)
+	if err != nil {
+		log.Println("File not found:", err)
+		log.Println("Creating new file...")
+		file, err = os.Create(app.FilePath)
+		if err != nil {
+			log.Println("Error creating file:", err)
+		}
+	}
+	defer file.Close()
 	fmt.Println("Server is running on port", port)
 	srv := &http.Server{
 		Addr: port,
 		Handler: routes(),
 	}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
