bae3c1decfc92b548ad7ae5481f96a8a154dfcec
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-26 10:55:48 +0200

    feat: create counter in 60 seconds

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,10 +1,25 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
+var (
+	requests []time.Time
+)
 
 func Counter(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	now := time.Now()
+	requests = append(requests, now)
+	cutoff := now.Add(-60 * time.Second)
+
+	count := 0
+	for _, req := range requests {
+		if req.After(cutoff) {
+			count++
+		}
+	}
+	fmt.Fprintf(w, "Number of requests in the last 60 seconds: %d\n", count)
 }
